4d0c43a5b8feaeea1d83d4bf380982eaeb86b782
olso <olsansky.martin@gmail.com>
2022-01-02 12:50:18 +0100

    chore: Setup Tailwind and styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GlobalStyle = createGlobalStyle``;
+
+const App = () => (
+  <>
+    <ThemeProvider theme={{}}>
+      <GlobalStyle />
+      <div className="space-y-5">Hello world</div>
+    </ThemeProvider>
+  </>
+)
 
 export default App;
